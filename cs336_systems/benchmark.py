51712b080158898e67398bb5471bbff3784f3078
sungdk0822 <sungdk0822@naver.com>
2025-07-31 15:45:04 +0900

    update for detailed profiling

--- cs336_systems/benchmark.py
+++ cs336_systems/benchmark.py
@@ -17,69 +17,76 @@ def benchmark_pass(
     measurement_steps: int = 10,
     forward_pass_only: bool = False,
     do_compile: bool = False,
-    do_synchronize: bool = True,
     device: str = 'cuda'
 ) -> None:
     batch_size = 4
     vocab_size = 10000
-    model = TransformerLanguageModel(
-        *TransformerLanguageModelConfig(
-            d_model=d_model,
-            num_heads=num_heads,
-            d_ff=d_ff,
-            vocab_size=vocab_size,
-            context_length=context_length,
-            num_layers=num_layers,
-            device=device
-        ).get_config()
-    )
+
+    with nvtx.range('define model'):
+        model = TransformerLanguageModel(
+            *TransformerLanguageModelConfig(
+                d_model=d_model,
+                num_heads=num_heads,
+                d_ff=d_ff,
+                vocab_size=vocab_size,
+                context_length=context_length,
+                num_layers=num_layers,
+                device=device
+            ).get_config()
+        )
+        if do_compile:
+            model = torch.compile(model)
 
     lr = 1e-3
-    optimizer = AdamW(model.parameters(), lr)
+    with nvtx.range('define optimizer'):
+        optimizer = AdamW(model.parameters(), lr)
 
-    if do_compile:
-        model = torch.compile(model)
+    with nvtx.range('define input'):
+        random_input_ids = torch.randint(0, vocab_size, (batch_size, context_length), device=device)
+        random_label_ids = torch.randint(0, vocab_size, (batch_size, context_length), device=device)
 
-    random_input_ids = torch.randint(0, vocab_size, (batch_size, context_length), device=device)
-    random_label_ids = torch.randint(0, vocab_size, (batch_size, context_length), device=device)
-
-    @nvtx.range('warmup')
-    def warmup():
-        for _ in range(warmup_steps):
-            lm_head_output = model.forward(random_input_ids)
-            if not forward_pass_only:
-                loss = cross_entropy(lm_head_output, random_label_ids)
-                loss.backward()
-                optimizer.step()
-        torch.cuda.synchronize()
-
-    @nvtx.range('measure')
     def measure():
+        if not forward_pass_only:
+            optimizer.zero_grad()
+        if forward_pass_only:
+            model.zero_grad()
+
         with nvtx.range('forward pass'):
             lm_head_output = model.forward(random_input_ids)   
-            if do_synchronize:
-                torch.cuda.synchronize()
 
         if not forward_pass_only:
             with nvtx.range('backward pass'):
                 loss = cross_entropy(lm_head_output, random_label_ids)
                 loss.backward()
-                if do_synchronize:
-                    torch.cuda.synchronize()
 
             with nvtx.range('optimizer step'):
                 optimizer.step()
-                if do_synchronize:
-                    torch.cuda.synchronize()
 
-    result = timeit.timeit(
-        stmt=measure,
-        setup=warmup,
-        number=measurement_steps,
-        globals=globals()
-    )
+    measured_times = []
 
-    print(f'{result / measurement_steps:.4f}')
+    for step in range(warmup_steps + measurement_steps):
+        if step < warmup_steps:
+            measure()
+
+        if step >= warmup_steps:
+            torch.cuda.cudart().cudaProfilerStart()
+
+            nvtx.range_push(f'step {step}')
+            torch.cuda.synchronize()
+            start_time = timeit.default_timer()
+            measure()
+            torch.cuda.synchronize()
+            end_time = timeit.default_timer()
+            nvtx.range_pop()
+
+            measured_times.append(end_time - start_time)
+
+    avg_time = sum(measured_times) / len(measured_times)
+
+    squared_times = [t ** 2 for t in measured_times]
+    std_time = ( sum(squared_times) / len(squared_times) - avg_time ** 2 ) ** 0.5
+
+    print(f'avg {avg_time:.4f} std {std_time:.4f}')
 
 
 @nvtx.range('scaled dot product attention')
@@ -153,7 +160,6 @@ if __name__ == '__main__':
     warmup_steps = 5
     forward_pass_only = False
     do_compile = False
-    do_synchronize = False
 
     for key, value in model_sizes.items():
         print(key)
@@ -162,7 +168,6 @@ if __name__ == '__main__':
             warmup_steps=warmup_steps, 
             forward_pass_only=forward_pass_only,
             do_compile=do_compile,
-            do_synchronize=do_synchronize,
             device=device
         )
 
