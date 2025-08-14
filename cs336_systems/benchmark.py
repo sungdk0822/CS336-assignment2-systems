cb3b4dd636249c4aafed2220a65d3a48c72d59df
sungdk0822 <sungdk0822@naver.com>
2025-08-14 09:46:15 +0000

    implement benchmark for FA2 and add benchmark results

--- cs336_systems/benchmark.py
+++ cs336_systems/benchmark.py
@@ -1,10 +1,11 @@
 import cs336_basics
+import timeit
 import torch
 import torch.cuda.nvtx as nvtx
-import timeit
-from contextlib import nullcontext
+import triton
 from cs336_basics.transformer_language_model import TransformerLanguageModel, softmax
 from cs336_basics.trainer import AdamW, TransformerLanguageModelConfig, cross_entropy
+from cs336_systems.flashattention import FlashAttention2
 from torch import inf
 from typing import Callable, Literal
 
@@ -291,44 +292,254 @@ def run_benchmark_attention(
     df = pd.DataFrame(results)
     pivot_df = df.pivot(index='d_model', columns='seq_len', values='avg_time')
     print(pivot_df)
-    '''
-        benchmark results on A100:
-            forward_pass_only=False
-            do_compile=False
-                seq_len     256       1024      4096      8192      16384
-                d_model                                                  
-                16       0.003059  0.002463  0.022000  0.081238  0.325173
-                32       0.002087  0.002555  0.022442  0.083447  0.333559
-                64       0.002075  0.002441  0.023391  0.087596  0.349094
-                128      0.002068  0.002496  0.025365  0.095230  0.378991
 
-            forward_pass_only=True
-            do_compile=False
-                seq_len     256       1024      4096      8192      16384
-                d_model                                                  
-                16       0.000943  0.000567  0.006108  0.022698  0.090291
-                32       0.000240  0.000532  0.006332  0.023609  0.093935
-                64       0.000242  0.000562  0.006788  0.025441  0.101489
-                128      0.000241  0.000622  0.007653  0.029206  0.116211
 
-            forward_pass_only=True
-            do_compile=True
-                seq_len     256       1024      4096      8192      16384
-                d_model                                                  
-                16       0.000238  0.000481  0.003631  0.013679  0.052226
-                32       0.000335  0.000448  0.004353  0.015626  0.056281
-                64       0.000306  0.000673  0.004820  0.016214  0.063881
-                128      0.000303  0.000733  0.005683  0.020036  0.078846
-    '''
+def benchmark_flashattention(
+    d_model: int,
+    seq_len: int,
+    dtype: torch.dtype,
+    forward_pass_only: bool
+) -> float:
+    batch_size = 1
+    is_causal = True
+
+    Q = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+    K = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+    V = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+
+    flash = torch.compile(FlashAttention2.apply)
+
+    def flash_forward_backward():
+        o = flash(Q, K, V, is_causal)
+        loss = o.sum()
+        loss.backward()
+
+    def flash_forward():
+        o = flash(Q, K, V, is_causal)
+
+    if forward_pass_only:
+        avg_time = triton.testing.do_bench(flash_forward, rep=100, warmup=10)
+    else:
+        avg_time = triton.testing.do_bench(flash_forward_backward, rep=100, warmup=10)
+    
+    return avg_time
+
+
+def run_benchmark_flashattention(
+    dtype: torch.dtype,
+    forward_pass_only: bool = False
+) -> None:
+    import pandas as pd
+    from itertools import product
+    d_models = [16, 32, 64, 128]
+    seq_lens = [256, 1024, 4096, 8192, 16384, 32768, 65536]
+
+    results = []
+    for d_model, seq_len in product(d_models, seq_lens):
+        # print(f'd_model {d_model} seq_len {seq_len}')
+        avg_time = benchmark_flashattention(
+            d_model,
+            seq_len,
+            dtype,
+            forward_pass_only
+        )
+        results.append({
+            'd_model': d_model,
+            'seq_len': seq_len,
+            'avg_time': avg_time
+        })
+    
+    df = pd.DataFrame(results)
+    pivot_df = df.pivot(index='d_model', columns='seq_len', values='avg_time')
+    pivot_df = pivot_df.round(3)
+    print(pivot_df)
+
+
+def benchmark_naive_attention(
+    d_model: int,
+    seq_len: int,
+    dtype: torch.dtype,
+    forward_pass_only: bool
+) -> float:
+    from cs336_basics.transformer_language_model import scaled_dot_product_attention
+    scaled_dot_product_attention = torch.compile(scaled_dot_product_attention)
+
+    batch_size = 1
+
+    Q = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+    K = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+    V = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=dtype, requires_grad=True)
+
+    def forward_backward():
+        o = scaled_dot_product_attention(Q, K, V)
+        loss = o.sum()
+        loss.backward()
+
+    def forward():
+        o = scaled_dot_product_attention(Q, K, V)
+
+    if forward_pass_only:
+        avg_time = triton.testing.do_bench(forward, rep=100, warmup=10)
+    else:
+        avg_time = triton.testing.do_bench(forward_backward, rep=100, warmup=10)
+    
+    return avg_time
+
+
+def run_benchmark_naive_attention(
+    dtype: torch.dtype,
+    forward_pass_only: bool = False
+) -> None:
+    import pandas as pd
+    from itertools import product
+    d_models = [16, 32, 64, 128]
+    seq_lens = [256, 1024, 4096, 8192, 16384, 32768, 65536]
+
+    results = []
+    for d_model, seq_len in product(d_models, seq_lens):
+        avg_time = benchmark_naive_attention(
+            d_model,
+            seq_len,
+            dtype,
+            forward_pass_only
+        )
+        results.append({
+            'd_model': d_model,
+            'seq_len': seq_len,
+            'avg_time': avg_time
+        })
+    
+    df = pd.DataFrame(results)
+    pivot_df = df.pivot(index='d_model', columns='seq_len', values='avg_time')
+    pivot_df = pivot_df.round(3)
+    print(pivot_df)
+
 
 if __name__ == '__main__':
+    import torch._dynamo as torchdynamo
+    torchdynamo.config.cache_size_limit = 32
+    torch.set_float32_matmul_precision('high')
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'using {device} device')
 
-    run_benchmark_attention(
-        forward_pass_only=True,
-        do_compile=True
+
+    run_benchmark_naive_attention(
+        torch.float32,
+        True
     )
+    '''
+    results on A100 (unit: ms):
+        seq_len  256    1024   4096   8192   16384   32768   65536
+        d_model                                                   
+        16       0.034  0.062  0.337  1.077  4.365  17.646  73.239
+        32       0.092  0.094  0.283  0.939  4.134  18.013  83.033
+        64       0.294  0.295  0.305  1.009  4.430  19.053  77.196
+        128      0.099  0.101  0.343  1.124  4.797  20.062  84.471
+    '''
+
+    run_benchmark_naive_attention(
+        torch.bfloat16,
+        True
+    )
+    '''
+    results on A100 (unit: ms):
+        seq_len  256    1024   4096   8192   16384   32768   65536
+        d_model                                                   
+        16       0.057  0.129  0.285  0.751  2.668  11.314  47.290
+        32       0.217  0.219  0.251  0.708  2.836  11.341  49.277
+        64       0.210  0.215  0.249  0.825  2.980  11.709  53.132
+        128      0.092  0.089  0.273  0.905  3.284  12.932  58.997
+    '''
+
+    run_benchmark_naive_attention(
+        torch.float32,
+        False
+    )
+    '''
+    results on A100 (unit: ms):
+        seq_len  256    1024   4096   8192    16384   32768
+        d_model                                            
+        16       0.858  1.853  2.257  3.159  12.835  52.198
+        32       2.318  2.122  2.112  3.034  12.522  52.708
+        64       1.520  2.064  2.107  3.203  13.279  55.941
+        128      1.068  1.674  2.329  3.392  14.092  66.127
+    '''
+    
+    run_benchmark_naive_attention(
+        torch.bfloat16,
+        False
+    )
+    '''
+    results on A100 (unit: ms):
+        seq_len  256    1024   4096   8192   16384   32768    65536
+        d_model                                                    
+        16       1.274  2.363  2.160  2.190  7.475  31.984  129.563
+        32       2.882  3.177  3.124  3.143  7.899  31.729  147.503
+        64       1.346  1.722  1.963  2.230  8.243  32.920  140.866
+        128      1.622  2.065  2.359  2.558  8.962  35.909  154.373
+    '''
+
+
+    run_benchmark_flashattention(
+        torch.float32,
+        True
+    )
+    '''
+    results on A100 (unit: ms):
+        (only the forward pass is implemented in Triton, the backward pass is implemented in PyTorch)
+        seq_len  256    1024   4096   8192   16384   32768   65536
+        d_model                                                   
+        16       0.012  0.029  0.099  0.201  0.552   1.933   7.423
+        32       0.015  0.036  0.125  0.272  0.775   2.741  11.165
+        64       0.023  0.073  0.291  0.652  2.435   8.630  32.312
+        128      0.027  0.086  0.351  1.331  4.100  14.810  58.941
+    '''
+
+    run_benchmark_flashattention(
+        torch.bfloat16,
+        True
+    )
+    '''
+    results on A100 (unit: ms):
+        (only the forward pass is implemented in Triton, the backward pass is implemented in PyTorch)
+        seq_len  256    1024   4096   8192   16384  32768   65536
+        d_model                                                  
+        16       0.017  0.032  0.090  0.165  0.474  1.423   8.240
+        32       0.211  0.176  0.174  0.361  0.917  2.043  10.514
+        64       0.141  0.144  0.153  0.267  0.739  2.599  15.113
+        128      0.172  0.177  0.324  0.829  2.311  6.703  19.919
+    '''
+
+    run_benchmark_flashattention(
+        torch.float32,
+        False
+    )
+    '''
+    results on A100 (unit: ms):
+        (only the forward pass is implemented in Triton, the backward pass is implemented in PyTorch)
+        seq_len  256    1024   4096   8192    16384   32768
+        d_model                                            
+        16       1.132  1.673  1.997  1.855   7.001  28.611
+        32       2.035  2.365  2.347  2.498   8.546  27.660
+        64       2.032  2.423  2.431  3.470   9.624  36.845
+        128      2.733  2.398  2.263  4.923  16.023  45.797
+    '''
+
+    run_benchmark_flashattention(
+        torch.bfloat16,
+        False
+    )
+    '''
+    results on A100 (unit: ms):
+        (only the forward pass is implemented in Triton, the backward pass is implemented in PyTorch)
+        seq_len  256    1024   4096   8192   16384   32768   65536
+        d_model                                                   
+        16       1.200  1.442  1.612  1.702  4.368  16.018  64.638
+        32       1.486  1.583  1.922  2.230  4.797  19.954  68.294
+        64       1.957  1.132  2.246  2.370  6.417  18.094  74.532
+        128      1.397  1.255  1.394  2.461  9.207  24.168  96.307
+    '''
 
 
 # uv run nsys profile -o result --force-overwrite true python cs336_systems/benchmark.py
