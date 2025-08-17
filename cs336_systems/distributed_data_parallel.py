b4910afd2a8bbeb399bdeccd77d2c13a37c3072d
sungdk0822 <sungdk0822@naver.com>
2025-08-17 16:00:47 +0900

    implement naive DDP benckmark

--- cs336_systems/distributed_data_parallel.py
+++ cs336_systems/distributed_data_parallel.py
@@ -1,9 +1,10 @@
 import os
 import timeit
-from turtle import forward
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
+from cs336_basics.transformer_language_model import TransformerLanguageModel
+from cs336_basics.trainer import AdamW, cross_entropy
 from torch import nn
 from typing import Literal
 
@@ -65,6 +66,96 @@ def benchmark_all_reduce(
     mp.spawn(fn=distributed_demo, args=(num_processes, data_size, backend), nprocs=num_processes, join=True)
 
 
+def naive_DDP(rank: int, world_size: int) -> None:
+    # d_model = 1600
+    # d_ff = 6400
+    # num_layers = 48
+    # num_heads = 25
+    # context_length = 256
+    # vocab_size = 10000
+    # batch_size = 100
+    d_model = 16
+    d_ff = 64
+    num_layers = 4
+    num_heads = 2
+    context_length = 256
+    vocab_size = 100
+    batch_size = 10
+    local_batch_size = batch_size // world_size
+    if torch.cuda.is_available() and world_size <= torch.cuda.device_count():
+        backend = 'nccl'
+        device = f'cuda:{rank}'
+    else:
+        backend = 'gloo'
+        device = 'cpu'
+    if rank == 0:
+        print(
+            f'gpu count: {torch.cuda.device_count()}', 
+            f'world size: {world_size}',
+            f'backend: {backend}', 
+            f'device: {device}',
+            sep='\n'
+        )
+
+    setup(rank, world_size, backend=backend)
+    torch.manual_seed(2025)
+
+    model = DDPIndividualParameters(
+        TransformerLanguageModel(
+                d_model,
+                num_heads,
+                d_ff,
+                10000.0,
+                vocab_size,
+                context_length,
+                num_layers,
+                device,
+            )
+    )
+    optimizer = AdamW(model.parameters())
+    input_ids = torch.randint(0, vocab_size, (batch_size, context_length))
+    label_ids = torch.randint(0, vocab_size, (batch_size, context_length))
+
+    steps = 50
+    communication_time_sum = 0
+    torch.cuda.synchronize()
+    total_time_start = timeit.default_timer()
+    for _ in range(steps):
+        local_input_ids = input_ids[rank * local_batch_size : (rank + 1) * local_batch_size, :].to(device)
+        local_label_ids = label_ids[rank * local_batch_size : (rank + 1) * local_batch_size, :].to(device)
+        optimizer.zero_grad()
+        lm_head_output = model(local_input_ids)
+        loss = cross_entropy(lm_head_output, local_label_ids)
+        loss.backward()
+
+        torch.cuda.synchronize()
+        communication_time_start = timeit.default_timer()
+        model.finish_gradient_synchronization()
+        torch.cuda.synchronize()
+        communication_time_end = timeit.default_timer()
+        communication_time_sum += (communication_time_end - communication_time_start)
+
+        optimizer.step()
+        permutation = torch.randperm(batch_size)
+        input_ids = input_ids[permutation]
+        label_ids = label_ids[permutation]
+    torch.cuda.synchronize()
+    total_time_end = timeit.default_timer()
+    total_time_per_step = (total_time_end - total_time_start) / steps
+    communication_time_per_step = communication_time_sum / steps
+    
+    if rank == 0:
+        print(f'total time per step: {total_time_per_step:6f}')
+        print(f'communication time per step: {communication_time_per_step:6f}')
+
+
+def benchmark_naive_DDP(
+
+) -> None:
+    world_size = 2
+    mp.spawn(naive_DDP, args=(world_size,), nprocs=world_size, join=True)
+
+
 # uv run pytest -k test_DistributedDataParallelIndividualParameters
 class DDPIndividualParameters(nn.Module):
     def __init__(self, module: nn.Module) -> None:
@@ -85,4 +176,4 @@ class DDPIndividualParameters(nn.Module):
 
 
 if __name__ == '__main__':
-    benchmark_all_reduce('nccl', 1000, 2)
+    benchmark_naive_DDP()
